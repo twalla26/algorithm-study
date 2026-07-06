0c55266c01cf4f25ea8270526065bfb4b7b7b37e
twalla26 <ssm4748@naver.com>
2026-07-06 11:43:23 +0900

    week1/귤고르기/twalla

--- src/week1/귤고르기/twalla/Solution.java
+++ src/week1/귤고르기/twalla/Solution.java
@@ -1,13 +1,33 @@
-import java.io.*;
 import java.util.*;
 
-public class Solution {
-
-    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static StringBuilder sb = new StringBuilder();
-    public static StringTokenizer st;
-
-    public static void main(String[] args) throws Exception {
+class Solution {
+    public int solution(int k, int[] tangerine) {
         
+        int T = tangerine.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < T; i++) {
+            int tang = tangerine[i];
+            map.put(tang, map.getOrDefault(tang, 0) + 1);
+        }
+        
+        List<Integer> keySet = new ArrayList<>(map.keySet());
+    
+        keySet.sort((o1, o2) -> {
+            return map.get(o2).compareTo(map.get(o1));
+        });
+        
+        int answer = 0;
+        
+        for (int key : keySet) {
+            if (k <= 0) {
+                break;
+            }
+            k -= map.get(key);
+            map.put(key, 0);
+            answer += 1;
+        }
+        
+        return answer;
     }
 }
