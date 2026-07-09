cd524341d53d6d88fca0874d3bec3bf7593ca552
twalla26 <ssm4748@naver.com>
2026-07-09 15:01:40 +0900

    week1/퍼즐게임챌린지/twalla 풀이 작성

--- src/week1/퍼즐게임챌린지/twalla/Solution.java
+++ src/week1/퍼즐게임챌린지/twalla/Solution.java
@@ -1,3 +1,20 @@
+// ✏️ 프로그래머스 퍼즐 게임 챌린지
+
+// 📶 문제 난이도
+// Level 2
+
+// 🔗 문제 링크
+// https://school.programmers.co.kr/learn/courses/30/lessons/340212#
+
+// ⏱️ 풀이 시간
+// 40분
+
+// ✅ 풀이 근거
+// 처음에 완전 탐색을 떠올려봤는데, 시간 초과가 날 것 같아서 이분 탐색으로 접근함.
+// 다만 첫 문제를 틀렸으면 어떻게 처리해야할 지에 대해 계속 고민했는데, 
+// 알고보니 입력으로는 항상 첫 문제의 diff가 1이라서 그냥 넘어가도 됐음.
+// (프로그래머스는 Level 2에서도 이분 탐색이 나오는 군요..!)
+
 class Solution {
     public int solution(int[] diffs, int[] times, long limit) {
         
